ed0bad4d725dece0720bc336ed11a0379b41e4b5
Stephen Li <stephenli.ca@gmail.com>
2021-09-02 19:49:25 -0400

    Check only renderAfterEvent xor renderAfterTime is set

--- src/PuppeteerPrerenderPluginOptions.ts
+++ src/PuppeteerPrerenderPluginOptions.ts
@@ -34,85 +34,106 @@ export interface PuppeteerPrerenderPluginOptions {
 }
 
 /* eslint-disable quote-props */
-const schema = {
-    type: 'object',
-    properties: {
-        'routes': {
-            type: 'array',
-            items: {
-                type: 'string',
-            },
-        },
-        'entryDir': {
+const baseProperties = {
+    'routes': {
+        type: 'array',
+        items: {
             type: 'string',
-        },
-        'entryFile': {
-            type: 'string',
-        },
-        'publicPath': {
-            type: 'string',
-        },
-        'outputDir': {
-            type: 'string',
-        },
-        'enabled': {
-            type: 'boolean',
-        },
-        'keepAlive': {
-            type: 'boolean',
-        },
-        'enablePageJs': {
-            type: 'boolean',
-        },
-        'maxConcurrent': {
-            type: 'integer',
-            minimum: 1,
-        },
-        'discoverNewRoutes': {
-            type: 'boolean',
-        },
-        'renderFirstRouteAlone': {
-            type: 'boolean',
-        },
-        'injections': {
-            type: 'array',
-            items: {
-                type: 'object',
-                properties: {
-                    'key': {
-                        type: 'string',
-                    },
-                    'value': {
-                        anyOf: [
-                            { type: 'number' },
-                            { type: 'string' },
-                            { type: 'boolean' },
-                            { type: 'array' },
-                            { type: 'object' },
-                        ],
-                    },
-                },
-                additionalProperties: false,
-            },
-        },
-        'renderAfterEvent': {
-            type: 'string',
-        },
-        'renderAfterTime': {
-            type: 'integer',
-            minimum: 0,
-        },
-        'postProcess': {},
-        'puppeteerOptions': {
-            type: 'object',
         },
     },
+    'entryDir': {
+        type: 'string',
+    },
+    'entryFile': {
+        type: 'string',
+    },
+    'publicPath': {
+        type: 'string',
+    },
+    'outputDir': {
+        type: 'string',
+    },
+    'enabled': {
+        type: 'boolean',
+    },
+    'keepAlive': {
+        type: 'boolean',
+    },
+    'enablePageJs': {
+        type: 'boolean',
+    },
+    'maxConcurrent': {
+        type: 'integer',
+        minimum: 1,
+    },
+    'discoverNewRoutes': {
+        type: 'boolean',
+    },
+    'renderFirstRouteAlone': {
+        type: 'boolean',
+    },
+    'injections': {
+        type: 'array',
+        items: {
+            type: 'object',
+            properties: {
+                'key': {
+                    type: 'string',
+                },
+                'value': {
+                    anyOf: [
+                        { type: 'number' },
+                        { type: 'string' },
+                        { type: 'boolean' },
+                        { type: 'array' },
+                        { type: 'object' },
+                    ],
+                },
+            },
+            additionalProperties: false,
+        },
+    },
+    'postProcess': {},
+    'puppeteerOptions': {
+        type: 'object',
+    },
+}
+
+const baseSchema = {
+    type: 'object',
     required: ['routes', 'entryDir'],
     additionalProperties: false,
 }
 
 const ajv = new Ajv()
-const validator = ajv.compile(schema)
+const validator = ajv.compile({
+    type: 'object',
+    anyOf: [
+        {
+            ...baseSchema,
+            properties: baseProperties,
+        },
+        {
+            ...baseSchema,
+            properties: {
+                ...baseProperties,
+                'renderAfterEvent': {
+                    type: 'string',
+                },
+            },
+        },
+        {
+            ...baseSchema,
+            properties: {
+                ...baseProperties,
+                'renderAfterTime': {
+                    type: 'integer',
+                    minimum: 0,
+                },
+            },
+        },
+    ],
+})
 
 export function isValidOptions(options: unknown): options is PuppeteerPrerenderPluginOptions {
     const isValid = validator(options)
