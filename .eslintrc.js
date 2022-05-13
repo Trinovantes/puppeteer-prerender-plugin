a8c676c0936414c6b67f48597d5ecd4adaa2fec5
Stephen Li <stephenli.ca@gmail.com>
2022-05-13 04:23:46 -0400

    Enable eslint browser env

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
 
     // Predefines global variables (e.g. browser env predefines 'window' variable)
     env: {
-        browser: false,
+        browser: true,
         node: true,
         'vue/setup-compiler-macros': true,
     },
